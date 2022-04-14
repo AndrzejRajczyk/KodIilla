d1eae82dd400a75fc3e239414a775f0b1036a037
AndrzejRajczyk <rajczyk.andrzej@gmail.com>
2022-04-14 16:48:56 +0200

    Modul 7.3

--- kodilla-stream/src/main/java/stream/StreamMain.java
+++ kodilla-stream/src/main/java/stream/StreamMain.java
@@ -1,38 +1,29 @@
-package com.kodilla.stream;                                                   // [1]
+package com.kodilla.stream;
 
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import stream.beautifier.PoemBeautifier;
+import stream.book.Book;
+import stream.book.BookDirectory;
+import stream.forumUser.Forum;
+import stream.forumUser.ForumUser;
 
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class StreamMain {                                                     // [3]
+public class StreamMain {
 
-    public static void main(String[] args) {                                   // [4]
-        ExpressionExecutor expressionExecutor = new ExpressionExecutor();       // [5]
-
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);           // [6]
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);           // [7]
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);           // [8]
-        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);   // [9]
-
-        System.out.println("Calculating expressions with method references");
-        expressionExecutor.executeExpression(3,4,  com.kodilla.stream.reference.FunctionalCalculator::multiplyAByB);
-        expressionExecutor.executeExpression(3, 4, com.kodilla.stream.reference.FunctionalCalculator::addAToB);
-        expressionExecutor.executeExpression(3, 4, com.kodilla.stream.reference.FunctionalCalculator::subBFromA);
-        expressionExecutor.executeExpression(3, 4, com.kodilla.stream.reference.FunctionalCalculator::divideAByB);
-
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
+    public static void main(String[] args) {
 
 
+        Forum forum = new Forum();
+        Map<String, ForumUser> theResultForum= forum.getUsersList().stream()
+                .filter(forumUser -> forumUser.getSex()=='M')
+                .filter(forumUser -> forumUser.getAge()>19)
+                .filter(forumUser ->forumUser.getUserPostQuantity()>0)
+                .collect(Collectors.toMap(forumUser->forumUser.getUserId(), forumUser->forumUser));
 
-        poemBeautifier.beautify(" Poem ",s -> "ABC"+s+"ABC");
-        poemBeautifier.beautify(" Poem ",s->s.toLowerCase());
-        poemBeautifier.beautify(" Poem ", s->s.toUpperCase());
-        poemBeautifier.beautify(" Poem ", s->s.trim());
+                    System.out.println("Filtred collection of Users: #"+theResultForum.size());
+                    theResultForum.entrySet().stream()
+                            .map(entry->"Key value: :" + entry.getKey() + " " + entry.getValue())
+                            .forEach(System.out::println);
 
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        com.kodilla.stream.iterate.NumbersGenerator.generateEven(20);
     }
-
-
 }
-
